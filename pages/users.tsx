86dbf5b3c61012c22fc9b259fd2c93632f0fefa3
Mikolaj Marcoll <mikolaj.marcoll@htdevelopers.com>
2022-02-20 15:00:14 +0100

    Week 1

---
+++ pages/users.tsx
@@ -0,0 +1,16 @@
+const UsersPage = () => {
+    return (
+        <div>user page - file</div>
+    )
+}
+// UserPage from file `users.tsx` is being displayed
+/*
+    event - compiled client and server successfully in 128 ms (142 modules)
+    warn  - Duplicate page detected. pages/users.tsx and pages/users/index.tsx both resolve to /users.
+    warn  - Duplicate page detected. pages/users.tsx and pages/users/index.tsx both resolve to /users.
+    wait  - compiling /users (client and server)...
+*/
+
+
+
+export default UsersPage
---
+++ pages/users/index.tsx
@@ -0,0 +1,9 @@
+const UsersPage = () => {
+    return (
+        <div>user page - directory</div>
+    )
+}
+
+
+
+export default UsersPage
